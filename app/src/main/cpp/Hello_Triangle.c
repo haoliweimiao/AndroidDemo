1394e6569996ee38f86b11ea68c4f18c9e1918d2
haoliwei <haoliweiwork@gmail.com>
2020-05-17 20:20:25 +0800

    Version: 1.0.40 [+]添加ndk读取assets文件方法

--- app/src/main/cpp/Hello_Triangle.c
+++ app/src/main/cpp/Hello_Triangle.c
@@ -36,187 +36,236 @@
 //    example is to demonstrate the basic concepts of
 //    OpenGL ES 3.0 rendering.
 #include "esUtil.h"
+#include <android/log.h>
+#include <android_native_app_glue.h>
+#include <android/asset_manager_jni.h>
+#include <android/asset_manager.h>
 
-typedef struct
-{
-   // Handle to a program object
-   GLuint programObject;
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "esUtil", __VA_ARGS__))
+
+typedef struct {
+    // Handle to a program object
+    GLuint programObject;
 
 } UserData;
+
+char *getAssetsFile(AAssetManager *mgr, const char *filename);
 
 ///
 // Create a shader object, load the shader source, and
 // compile the shader.
 //
-GLuint LoadShader ( GLenum type, const char *shaderSrc )
-{
-   GLuint shader;
-   GLint compiled;
+GLuint LoadShader(GLenum type, const char *shaderSrc) {
+    GLuint shader;
+    GLint compiled;
 
-   // Create the shader object
-   shader = glCreateShader ( type );
+    // Create the shader object
+    shader = glCreateShader(type);
 
-   if ( shader == 0 )
-   {
-      return 0;
-   }
+    if (shader == 0) {
+        return 0;
+    }
 
-   // Load the shader source
-   glShaderSource ( shader, 1, &shaderSrc, NULL );
+    // Load the shader source
+    glShaderSource(shader, 1, &shaderSrc, NULL);
 
-   // Compile the shader
-   glCompileShader ( shader );
+    // Compile the shader
+    glCompileShader(shader);
 
-   // Check the compile status
-   glGetShaderiv ( shader, GL_COMPILE_STATUS, &compiled );
+    // Check the compile status
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
 
-   if ( !compiled )
-   {
-      GLint infoLen = 0;
+    if (!compiled) {
+        GLint infoLen = 0;
 
-      glGetShaderiv ( shader, GL_INFO_LOG_LENGTH, &infoLen );
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
 
-      if ( infoLen > 1 )
-      {
-         char *infoLog = malloc ( sizeof ( char ) * infoLen );
+        if (infoLen > 1) {
+            char *infoLog = malloc(sizeof(char) * infoLen);
 
-         glGetShaderInfoLog ( shader, infoLen, NULL, infoLog );
-         esLogMessage ( "Error compiling shader:\n%s\n", infoLog );
+            glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
+            esLogMessage("Error compiling shader:\n%s\n", infoLog);
 
-         free ( infoLog );
-      }
+            free(infoLog);
+        }
 
-      glDeleteShader ( shader );
-      return 0;
-   }
+        glDeleteShader(shader);
+        return 0;
+    }
 
-   return shader;
+    return shader;
 
 }
 
 ///
 // Initialize the shader and program object
 //
-int Init ( ESContext *esContext )
-{
-   UserData *userData = esContext->userData;
-   char vShaderStr[] =
-      "#version 300 es                          \n"
-      "layout(location = 0) in vec4 vPosition;  \n"
-      "void main()                              \n"
-      "{                                        \n"
-      "   gl_Position = vPosition;              \n"
-      "}                                        \n";
+int Init(ESContext *esContext) {
+    UserData *userData = esContext->userData;
+    char vShaderStr[] =
+            "#version 300 es                          \n"
+            "layout(location = 0) in vec4 vPosition;  \n"
+            "void main()                              \n"
+            "{                                        \n"
+            "   gl_Position = vPosition;              \n"
+            "}                                        \n";
 
-   char fShaderStr[] =
-      "#version 300 es                              \n"
-      "precision mediump float;                     \n"
-      "out vec4 fragColor;                          \n"
-      "void main()                                  \n"
-      "{                                            \n"
-      "   fragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );  \n"
-      "}                                            \n";
+    char fShaderStr[] =
+            "#version 300 es                              \n"
+            "precision mediump float;                     \n"
+            "out vec4 fragColor;                          \n"
+            "void main()                                  \n"
+            "{                                            \n"
+            "   fragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );  \n"
+            "}                                            \n";
 
-   GLuint vertexShader;
-   GLuint fragmentShader;
-   GLuint programObject;
-   GLint linked;
+    GLuint vertexShader;
+    GLuint fragmentShader;
+    GLuint programObject;
+    GLint linked;
 
-   // Load the vertex/fragment shaders
-   vertexShader = LoadShader ( GL_VERTEX_SHADER, vShaderStr );
-   fragmentShader = LoadShader ( GL_FRAGMENT_SHADER, fShaderStr );
+    // Load the vertex/fragment shaders
+    vertexShader = LoadShader(GL_VERTEX_SHADER, vShaderStr);
+    fragmentShader = LoadShader(GL_FRAGMENT_SHADER, fShaderStr);
 
-   // Create the program object
-   programObject = glCreateProgram ( );
+    // Create the program object
+    programObject = glCreateProgram();
 
-   if ( programObject == 0 )
-   {
-      return 0;
-   }
+    if (programObject == 0) {
+        return 0;
+    }
 
-   glAttachShader ( programObject, vertexShader );
-   glAttachShader ( programObject, fragmentShader );
+    glAttachShader(programObject, vertexShader);
+    glAttachShader(programObject, fragmentShader);
 
-   // Link the program
-   glLinkProgram ( programObject );
+    // Link the program
+    glLinkProgram(programObject);
 
-   // Check the link status
-   glGetProgramiv ( programObject, GL_LINK_STATUS, &linked );
+    // Check the link status
+    glGetProgramiv(programObject, GL_LINK_STATUS, &linked);
 
-   if ( !linked )
-   {
-      GLint infoLen = 0;
+    if (!linked) {
+        GLint infoLen = 0;
 
-      glGetProgramiv ( programObject, GL_INFO_LOG_LENGTH, &infoLen );
+        glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, &infoLen);
 
-      if ( infoLen > 1 )
-      {
-         char *infoLog = malloc ( sizeof ( char ) * infoLen );
+        if (infoLen > 1) {
+            char *infoLog = malloc(sizeof(char) * infoLen);
 
-         glGetProgramInfoLog ( programObject, infoLen, NULL, infoLog );
-         esLogMessage ( "Error linking program:\n%s\n", infoLog );
+            glGetProgramInfoLog(programObject, infoLen, NULL, infoLog);
+            esLogMessage("Error linking program:\n%s\n", infoLog);
 
-         free ( infoLog );
-      }
+            free(infoLog);
+        }
 
-      glDeleteProgram ( programObject );
-      return FALSE;
-   }
+        glDeleteProgram(programObject);
+        return FALSE;
+    }
 
-   // Store the program object
-   userData->programObject = programObject;
+    // Store the program object
+    userData->programObject = programObject;
 
-   glClearColor ( 1.0f, 1.0f, 1.0f, 0.0f );
-   return TRUE;
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    return TRUE;
 }
 
 ///
 // Draw a triangle using the shader pair created in Init()
 //
-void Draw ( ESContext *esContext )
-{
-   UserData *userData = esContext->userData;
-   GLfloat vVertices[] = {  0.0f,  0.5f, 0.0f,
-                            -0.5f, -0.5f, 0.0f,
-                            0.5f, -0.5f, 0.0f
-                         };
+void Draw(ESContext *esContext) {
+    UserData *userData = esContext->userData;
+    GLfloat vVertices[] = {0.0f, 0.5f, 0.0f,
+                           -0.5f, -0.5f, 0.0f,
+                           0.5f, -0.5f, 0.0f
+    };
 
-   // Set the viewport
-   glViewport ( 0, 0, esContext->width, esContext->height );
+    // Set the viewport
+    glViewport(0, 0, esContext->width, esContext->height);
 
-   // Clear the color buffer
-   glClear ( GL_COLOR_BUFFER_BIT );
+    // Clear the color buffer
+    glClear(GL_COLOR_BUFFER_BIT);
 
-   // Use the program object
-   glUseProgram ( userData->programObject );
+    // Use the program object
+    glUseProgram(userData->programObject);
 
-   // Load the vertex data
-   glVertexAttribPointer ( 0, 3, GL_FLOAT, GL_FALSE, 0, vVertices );
-   glEnableVertexAttribArray ( 0 );
+    // Load the vertex data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vVertices);
+    glEnableVertexAttribArray(0);
 
-   glDrawArrays ( GL_TRIANGLES, 0, 3 );
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
-void Shutdown ( ESContext *esContext )
-{
-   UserData *userData = esContext->userData;
+void Shutdown(ESContext *esContext) {
+    UserData *userData = esContext->userData;
 
-   glDeleteProgram ( userData->programObject );
+    glDeleteProgram(userData->programObject);
 }
 
-int esMain ( ESContext *esContext )
-{
-   esContext->userData = malloc ( sizeof ( UserData ) );
+///
+// Load texture from disk
+//
+GLuint LoadTexture(void *ioContext, char *fileName) {
+    int width,
+            height;
 
-   esCreateWindow ( esContext, "Hello Triangle", 320, 240, ES_WINDOW_RGB );
+    char *buffer = esLoadTGA(ioContext, fileName, &width, &height);
+    GLuint texId;
 
-   if ( !Init ( esContext ) )
-   {
-      return GL_FALSE;
-   }
+    if (buffer == NULL) {
+        esLogMessage("Error loading (%s) image.\n", fileName);
+        return 0;
+    }
 
-   esRegisterShutdownFunc ( esContext, Shutdown );
-   esRegisterDrawFunc ( esContext, Draw );
+    glGenTextures(1, &texId);
+    glBindTexture(GL_TEXTURE_2D, texId);
 
-   return GL_TRUE;
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, buffer);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    free(buffer);
+
+    return texId;
+}
+
+int esMain(ESContext *esContext) {
+    esContext->userData = malloc(sizeof(UserData));
+
+    esCreateWindow(esContext, "Hello Triangle", 320, 240, ES_WINDOW_RGB);
+
+    if (!Init(esContext)) {
+        return GL_FALSE;
+    }
+
+    char *data = getAssetsFile(esContext->platformData, "test.txt");
+    esLogMessage("load text file in android assets:\n%s\n", data);
+
+    esRegisterShutdownFunc(esContext, Shutdown);
+    esRegisterDrawFunc(esContext, Draw);
+
+    return GL_TRUE;
+}
+
+char *getAssetsFile(AAssetManager *mgr, const char *filename) {
+    //open file
+    AAsset *assetFile = AAssetManager_open(mgr, filename, AASSET_MODE_BUFFER);
+    //this will also be ok
+    //AAsset *assetFile = AAssetManager_open(nativeasset, "dir/cs.jpg", AASSET_MODE_BUFFER);
+    //get file length
+    size_t fileLength = (size_t) AAsset_getLength(assetFile);
+    char *fileData = (char *) malloc(fileLength);
+    //read file data
+    AAsset_read(assetFile, fileData, fileLength);
+    //the data has been copied to dataBuffer2, so , close it
+    AAsset_close(assetFile);
+
+    // +1 because of '\0' at the end
+    char *data = malloc(strlen(fileData) + 1);
+    strcpy(data, fileData);
+    //free malloc
+    free(fileData);
+
+    return data;
 }
