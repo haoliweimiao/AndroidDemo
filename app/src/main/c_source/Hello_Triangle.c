036dd2ae81ed436fe0ab4834f6f9223ed878d285
haoliwei <haoliweiwork@gmail.com>
2020-05-19 22:16:32 +0800

    Version: 1.0.42 [+]添加 方块绘制方法

--- app/src/main/c_source/Hello_Triangle.c
+++ app/src/main/c_source/Hello_Triangle.c
@@ -1,4 +1,5 @@
 #include "esUtil.h"
+#include "LinkUtil.h"
 #include <android/log.h>
 #include <android_native_app_glue.h>
 #include <android/asset_manager_jni.h>
@@ -11,54 +12,6 @@ typedef struct {
     GLuint programObject;
 
 } UserData;
-
-char *getAssetsFile(AAssetManager *mgr, const char *filename);
-
-///
-// Create a shader object, load the shader source, and
-// compile the shader.
-//
-GLuint LoadShader(GLenum type, const char *shaderSrc) {
-    GLuint shader;
-    GLint compiled;
-
-    // Create the shader object
-    shader = glCreateShader(type);
-
-    if (shader == 0) {
-        return 0;
-    }
-
-    // Load the shader source
-    glShaderSource(shader, 1, &shaderSrc, NULL);
-
-    // Compile the shader
-    glCompileShader(shader);
-
-    // Check the compile status
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-
-    if (!compiled) {
-        GLint infoLen = 0;
-
-        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
-
-        if (infoLen > 1) {
-            char *infoLog = malloc(sizeof(char) * infoLen);
-
-            glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
-            esLogMessage("Error compiling shader:\n%s\n", infoLog);
-
-            free(infoLog);
-        }
-
-        glDeleteShader(shader);
-        return 0;
-    }
-
-    return shader;
-
-}
 
 ///
 // Initialize the shader and program object
@@ -81,38 +34,7 @@ int Init(ESContext *esContext) {
     fragmentShader = LoadShader(GL_FRAGMENT_SHADER, fShaderStr);
 
     // Create the program object
-    programObject = glCreateProgram();
-
-    if (programObject == 0) {
-        return 0;
-    }
-
-    glAttachShader(programObject, vertexShader);
-    glAttachShader(programObject, fragmentShader);
-
-    // Link the program
-    glLinkProgram(programObject);
-
-    // Check the link status
-    glGetProgramiv(programObject, GL_LINK_STATUS, &linked);
-
-    if (!linked) {
-        GLint infoLen = 0;
-
-        glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, &infoLen);
-
-        if (infoLen > 1) {
-            char *infoLog = malloc(sizeof(char) * infoLen);
-
-            glGetProgramInfoLog(programObject, infoLen, NULL, infoLog);
-            esLogMessage("Error linking program:\n%s\n", infoLog);
-
-            free(infoLog);
-        }
-
-        glDeleteProgram(programObject);
-        return FALSE;
-    }
+    programObject = linkProgram(&vertexShader, &fragmentShader, &linked);
 
     // Store the program object
     userData->programObject = programObject;
@@ -153,35 +75,6 @@ void Shutdown(ESContext *esContext) {
     glDeleteProgram(userData->programObject);
 }
 
-///
-// Load texture from disk
-//
-GLuint LoadTexture(void *ioContext, char *fileName) {
-    int width,
-            height;
-
-    char *buffer = esLoadTGA(ioContext, fileName, &width, &height);
-    GLuint texId;
-
-    if (buffer == NULL) {
-        esLogMessage("Error loading (%s) image.\n", fileName);
-        return 0;
-    }
-
-    glGenTextures(1, &texId);
-    glBindTexture(GL_TEXTURE_2D, texId);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, buffer);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-    free(buffer);
-
-    return texId;
-}
-
 int esMain(ESContext *esContext) {
     esContext->userData = malloc(sizeof(UserData));
 
@@ -191,33 +84,8 @@ int esMain(ESContext *esContext) {
         return GL_FALSE;
     }
 
-//    char *data = getAssetsFile(esContext->platformData, "test.txt");
-//    esLogMessage("load text file in android assets:\n%s\n", data);
-
     esRegisterShutdownFunc(esContext, Shutdown);
     esRegisterDrawFunc(esContext, Draw);
 
     return GL_TRUE;
-}
-
-char *getAssetsFile(AAssetManager *mgr, const char *filename) {
-    //open file
-    AAsset *assetFile = AAssetManager_open(mgr, filename, AASSET_MODE_BUFFER);
-    //this will also be ok
-    //AAsset *assetFile = AAssetManager_open(nativeasset, "dir/cs.jpg", AASSET_MODE_BUFFER);
-    //get file length
-    size_t fileLength = (size_t) AAsset_getLength(assetFile);
-    char *fileData = (char *) malloc(fileLength);
-    //read file data
-    AAsset_read(assetFile, fileData, fileLength);
-    //the data has been copied to dataBuffer2, so , close it
-    AAsset_close(assetFile);
-
-    // +1 because of '\0' at the end
-    char *data = malloc(strlen(fileData) + 1);
-    strcpy(data, fileData);
-    //free malloc
-    free(fileData);
-
-    return data;
 }
--- app/src/main/cpp/DrawMain.c
+++ app/src/main/cpp/DrawMain.c
@@ -13,15 +13,17 @@ typedef struct {
 
 } UserData;
 
+unsigned int VBO, VAO, EBO;
+
 ///
 // Initialize the shader and program object
 //
 int Init(ESContext *esContext) {
     UserData *userData = esContext->userData;
-    char *vShaderStr = getAssetsFile(esContext->platformData, "glsl/triangle/vertex_3_0.glsl");
+    char *vShaderStr = getAssetsFile(esContext->platformData, "glsl/rectangle/vertex.glsl");
     esLogMessage("load vertex text file in android assets:\n%s\n", vShaderStr);
 
-    char *fShaderStr = getAssetsFile(esContext->platformData, "glsl/triangle/fragment_3_0.glsl");
+    char *fShaderStr = getAssetsFile(esContext->platformData, "glsl/rectangle/fragment.glsl");
     esLogMessage("load fragment text file in android assets:\n%s\n", vShaderStr);
 
     GLuint vertexShader;
@@ -36,10 +38,60 @@ int Init(ESContext *esContext) {
     // Create the program object
     programObject = linkProgram(&vertexShader, &fragmentShader, &linked);
 
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+
     // Store the program object
     userData->programObject = programObject;
 
-    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    GLfloat vVertices[] = {
+            0.5f, 0.5f, 0.0f, // top right
+            0.5f, -0.5f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f, // bottom left
+            -0.5f, 0.5f, 0.0f  // top left
+    };
+    GLint indices[] = {
+            // note that we start from 0!
+            0, 1, 3, // first Triangle
+            1, 2, 3  // second Triangle
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vVertices), vVertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO
+    // as the vertex attribute's bound vertex buffer object so afterwards we can
+    // safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element
+    // buffer object IS stored in the VAO; keep the EBO bound.
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally
+    // modify this VAO, but this rarely happens. Modifying other VAOs requires a
+    // call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
+    // VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    // Set the viewport
+    glViewport(0, 0, esContext->width, esContext->height);
+
     return TRUE;
 }
 
@@ -48,25 +100,19 @@ int Init(ESContext *esContext) {
 //
 void Draw(ESContext *esContext) {
     UserData *userData = esContext->userData;
-    GLfloat vVertices[] = {0.0f, 0.5f, 0.0f,
-                           -0.5f, -0.5f, 0.0f,
-                           0.5f, -0.5f, 0.0f
-    };
 
-    // Set the viewport
-    glViewport(0, 0, esContext->width, esContext->height);
-
-    // Clear the color buffer
+    // render
+    // ------
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Use the program object
+    // draw our first triangle
     glUseProgram(userData->programObject);
-
-    // Load the vertex data
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vVertices);
-    glEnableVertexAttribArray(0);
-
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(
+            VAO); // seeing as we only have a single VAO there's no need to bind it
+    // every time, but we'll do so to keep things a bit more organized
+    // glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 void Shutdown(ESContext *esContext) {
