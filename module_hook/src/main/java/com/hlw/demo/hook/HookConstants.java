172600acf9a75b35abab18a7cc2cca8e08d63af5
von.wu <von.wu@zkteco.com>
2023-10-19 22:36:19 +0800

    Version: 1.0.74 [+]使用DexClassLoader读取其他APK常量示例

---
+++ module_hook/src/main/java/com/hlw/demo/hook/HookConstants.java
@@ -0,0 +1,8 @@
+package com.hlw.demo.hook;
+
+public interface HookConstants {
+    public String[] mHookStrings = new String[]{
+            "com.hlw.demo.hook.test1",
+            "com.hlw.demo.hook.test2",
+    };
+}
--- module_hook/src/main/java/com/hlw/demo/hook/HookMainActivity.java
+++ module_hook/src/main/java/com/hlw/demo/hook/HookMainActivity.java
@@ -1,19 +1,25 @@
 package com.hlw.demo.hook;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+import dalvik.system.DexClassLoader;
 
 public class HookMainActivity extends AppCompatActivity {
 
@@ -37,23 +43,81 @@ public class HookMainActivity extends AppCompatActivity {
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 测试AMS HOOK (调用其相关方法)
-                Uri uri = Uri.parse("http://wwww.baidu.com");
-                Intent t = new Intent(Intent.ACTION_VIEW);
-                t.setData(uri);
-                startActivity(t);
+                hookAMS();
+
+                hookApkMethod();
             }
         });
+
+        requestSDCardPermission();
+    }
+
+    private void hookAMS() {
+        // 测试AMS HOOK (调用其相关方法)
+        Uri uri = Uri.parse("http://www.baidu.com");
+        Intent t = new Intent(Intent.ACTION_VIEW);
+        t.setData(uri);
+        startActivity(t);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        HashMap<String, UsbDevice> deviceHashMap = ((UsbManager)
-                HookMainActivity.this.getSystemService(Context.USB_SERVICE)).getDeviceList();
-        for (Map.Entry<String, UsbDevice> entry : deviceHashMap.entrySet()) {
-            UsbDevice device = entry.getValue();
-            Log.d("usb device", String.format("usb device pid:%s vid:%s name:%s %s", device.getProductId(), device.getVendorId(), device.getDeviceName(), device.getProductName()));
+    }
+
+    private void requestSDCardPermission() {
+        String[] permission = {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> needRequestPermission = new ArrayList<>();
+            for (String per : permission) {
+                if (ActivityCompat.checkSelfPermission(this, permission[0]) != 0) {
+                    needRequestPermission.add(per);
+                }
+            }
+            if (needRequestPermission.size() > 0)
+                ActivityCompat.requestPermissions(this, permission, 99);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                startActivity(new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION));
+            }
+        }
+    }
+
+    private static final String WHITE_LIST_CLASS_NAME = "com.hlw.demo.hook.HookConstants";
+    private static final String WHITE_LIST_FIELD_NAME = "mHookStrings";
+
+    private void hookApkMethod() {
+        File root = new File(getApplication().getFilesDir(), "ManagerImplLoader");
+        File odexDir = new File(root, "10086");
+        odexDir.mkdirs();
+        // 需要把 module_hook 打包，并推送到 /sdcard/hook.apk
+        DexClassLoader dexClassLoader = new DexClassLoader(
+                "/sdcard/hook.apk",
+                odexDir.getAbsolutePath(),
+                null,
+                getClass().getClassLoader()
+        );
+
+        String[] mHookStrings = null;
+        try {
+            Class<?> whiteListClass = dexClassLoader.loadClass(WHITE_LIST_CLASS_NAME);
+            Field whiteListField = whiteListClass.getDeclaredField(WHITE_LIST_FIELD_NAME);
+            Object o = whiteListField.get(null);
+            mHookStrings = (String[]) o;
+            for (String test : mHookStrings) {
+                Log.e("!!!!!!!!", "test " + test);
+            }
+        } catch (ClassNotFoundException ignored) {
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
         }
     }
 }
